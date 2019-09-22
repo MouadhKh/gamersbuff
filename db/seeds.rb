d4c5fd4253734a0e36f346bd6233f8f8727bd6c5
0P1X <mo3.khlifi@gmail.com>
2019-09-22 10:07:24 +0100

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@ admin = User.create(
   }
 )
 admin.confirm
+
+opix = User.create(
+  {email: "mo3.khlifi@gmail.com",
+   username: "opix",
+   password: "123456",
+   password_confirmation: "123456"
+  }
+)
+opix.confirm
