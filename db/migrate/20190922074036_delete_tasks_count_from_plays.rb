e76501faed14007aa51d54120940a7e4c1bef5aa
0P1X <mo3.khlifi@gmail.com>
2019-09-22 10:15:28 +0100

    ignore migration

--- db/migrate/20190922074036_delete_tasks_count_from_plays.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTasksCountFromPlays < ActiveRecord::Migration[5.2]
-  def change
-  remove_column :plays, :tasks_count, :integer
-  end
-end
--- db/migrate/20190922083535_destroy_tasks_count_from_plays.rb
+++
@@ -1,5 +0,0 @@
-class DestroyTasksCountFromPlays < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :plays, :tasks_count
-  end
-end
