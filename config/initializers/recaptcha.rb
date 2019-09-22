35007ed3836a59a67994246ae081e8b77fcd9246
0P1X <mo3.khlifi@gmail.com>
2019-09-22 10:02:24 +0100

    added recaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,6 +3,6 @@
 #   config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 # end
 Recaptcha.configure do |config|
-  config.site_key = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-  config.secret_key = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+  config.site_key = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
